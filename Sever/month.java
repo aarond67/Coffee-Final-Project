343e81ce83ba302299d5273fc8e53ca0bbb99e25
Aaron D <76273345+aarond67@users.noreply.github.com>
2022-05-11 10:15:45 -0700

    making month class

--- Sever/month.java
+++ Sever/month.java
@@ -5,4 +5,14 @@ public class month
      income per day
      most popular 
      */
+     private String name;
+     private double totalIncome = 0;
+     private double incomePerDay = 0;
+     private String mostpop;
+     private barasitas[][] workers; 
+     public month(String name, int days){
+         this.name = name;
+         workers = new barasitas[3][days];
+     }
+    
 }
