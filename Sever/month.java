d42e2eae68d1006bfeab29e3300fdec043420045
Aaron Delgado <adelgado2431@gmail.com>
2022-05-17 19:34:29 -0700

    fix this

--- Sever/month.java
+++ Sever/month.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class month
 {
     /* barasita list for each day
@@ -10,21 +11,41 @@ public class month
      private double incomePerDay = 0;
      private String mostpop;
      private Day[] days; 
-     public month(String name, int days){
+     private ArrayList <String> listOfDrinks = new ArrayList<String> ();
+     public month(String name, int numDay){
+        this.name = name;
+        days = new Day[numDay];
+        listOfDrinks.add("Mocha");
+        listOfDrinks.add("Black");
+        listOfDrinks.add("Latte");
+        listOfDrinks.add("Cap");
+        for(int i = 0; i < days.length;i++){
+             Day newo = new Day(listOfDrinks);
+             days[i] = newo;
+         }
+    }
+    public month(String name, int numDay, ArrayList <String> newlistOfDrinks){
          this.name = name;
-         days = new Day[days]
-     }
-     public boolean addDay(day d1){
-        for(int i = 0; i < days.length; i++){
-            if(days[i] == null){
-                days[i] = d1;
-                return true;
-            }
+         days = new Day[numDay];
+         listOfDrinks = newlistOfDrinks;
+        for(int i = 0; i < days.length;i++){
+             Day newo = new Day(listOfDrinks);
+             days[i] = newo;
+         }
+    }
+    public Day beginDay(int num,String shift){
+        if(shift.equals("Morning")){
+            days[num-1].startDay(shift);
+            return days[num-1];
         }
-        return false;
-    }
-    public String popularDrink(){
+        System.out.println("WORNG");
         
+        return days[num-1];
     }
+    public String getName(){
+        return name;
+    }
+    
+    
     
 }
