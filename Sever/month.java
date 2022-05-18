d4f68c3a34d3b6fa1d11265b59eac31de342bb37
Aaron Delgado <adelgado2431@gmail.com>
2022-05-17 19:19:17 -0700

    yes

---
+++ Sever/month.java
@@ -0,0 +1,7 @@
+public class month {
+    public static void main(String[] args) {
+        year something = new year();
+        System.out.println(something.claculator(5));
+
+    }
+}
---
+++ Sever/year.java
@@ -0,0 +1,11 @@
+public class year{
+    public year(){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public int claculator(int i){
+        return i+1;
+    }
+}
